3015703faaca4fe210f331caddaa3baeb647bc32
cobra-PICH <124275918+cobra-PICH@users.noreply.github.com>
2026-04-06 09:20:28 +0700

    fix: lowercase site URL, fix gitignore, move react to devDeps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import pagefind from 'astro-pagefind';
 
 export default defineConfig({
-  site: 'https://cobra-PICH.github.io',
+  site: 'https://cobra-pich.github.io',
   base: '/ios-qa',
   integrations: [
     react(),
